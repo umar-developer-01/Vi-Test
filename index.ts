4d323fb2e89192dba563fc89d3aa03a467549b7f
umar-developer-01 <umarkhandev9@gmail.com>
2024-11-17 15:17:05 +0530

    return values from mocks

--- index.ts
+++ index.ts
@@ -6,59 +6,60 @@ export const app = express();
 app.use(express.json());
 
 const sumInput = z.object({
-    a: z.number(),
-    b: z.number()
+  a: z.number(),
+  b: z.number(),
 });
 
 app.post("/sum", async (req, res) => {
-    const parsedResponse = sumInput.safeParse(req.body);
+  const parsedResponse = sumInput.safeParse(req.body);
 
-    if (!parsedResponse.success) {
-        return res.status(411).json({
-            message: "Incorrect inputs"
-        });
-    }
-
-    const answer = parsedResponse.data.a + parsedResponse.data.b;
-
-    const response = await prismaClient.sum.create({
-        data: {
-            a: parsedResponse.data.a,
-            b: parsedResponse.data.b,
-            result: answer
-        }
+  if (!parsedResponse.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
     });
+  }
 
-    res.json({
-        answer,
-        id: response.id
-    });
+  const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+  const response = await prismaClient.sum.create({
+    data: {
+      a: parsedResponse.data.a,
+      b: parsedResponse.data.b,
+      result: answer,
+    },
+  });
+  
+
+  res.json({
+    answer,
+    id: response.id,
+  });
 });
 
 app.get("/sum", async (req, res) => {
-    const parsedResponse = sumInput.safeParse({
-        a: Number(req.headers["a"]),
-        b: Number(req.headers["b"])
+  const parsedResponse = sumInput.safeParse({
+    a: Number(req.headers["a"]),
+    b: Number(req.headers["b"]),
+  });
+
+  if (!parsedResponse.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
     });
+  }
 
-    if (!parsedResponse.success) {
-        return res.status(411).json({
-            message: "Incorrect inputs"
-        });
-    }
+  const answer = parsedResponse.data.a + parsedResponse.data.b;
 
-    const answer = parsedResponse.data.a + parsedResponse.data.b;
+  const response = await prismaClient.sum.create({
+    data: {
+      a: parsedResponse.data.a,
+      b: parsedResponse.data.b,
+      result: answer,
+    },
+  });
 
-    const response = await prismaClient.sum.create({
-        data: {
-            a: parsedResponse.data.a,
-            b: parsedResponse.data.b,
-            result: answer
-        }
-    });
-
-    res.json({
-        answer,
-        id: response.id
-    });
+  res.json({
+    answer,
+    id: response.id,
+  });
 });
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,25 +1,26 @@
-import { describe, expect, test, it, vi } from 'vitest';
+import { describe, expect, test, it, vi } from "vitest";
 import request from "supertest";
 import { app } from "../index";
-import { prismaClient } from '../__mocks__/db';
+import { prismaClient } from "../__mocks__/db";
 
-vi.mock('../db');
+vi.mock("../db");
 
 describe("POST /sum", () => {
   it("should return the sum of two numbers", async () => {
     prismaClient.sum.create.mockResolvedValue({
-      id: 1,
+      id: 5,
       a: 1,
       b: 1,
-      result: 3
+      result: 3,
     });
 
     const res = await request(app).post("/sum").send({
       a: 1,
-      b: 2
+      b: 2,
     });
     expect(res.statusCode).toBe(200);
     expect(res.body.answer).toBe(3);
+    expect(res.body.id).toBe(5);
   });
 
   it("should return 411 if no inputs are provided", async () => {
@@ -35,14 +36,14 @@ describe("GET /sum", () => {
       id: 1,
       a: 1,
       b: 1,
-      result: 3
+      result: 3,
     });
 
     const res = await request(app)
       .get("/sum")
       .set({
         a: "1",
-        b: "2"
+        b: "2",
       })
       .send();
     expect(res.statusCode).toBe(200);
@@ -50,8 +51,7 @@ describe("GET /sum", () => {
   });
 
   it("should return 411 if no inputs are provided", async () => {
-    const res = await request(app)
-      .get("/sum").send();
+    const res = await request(app).get("/sum").send();
     expect(res.statusCode).toBe(411);
   });
 });
