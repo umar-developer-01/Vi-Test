f1acaa6d6b8a5ded61cada1dbf2b771493d0d7c0
umar-developer-01 <umarkhandev9@gmail.com>
2024-11-17 15:52:47 +0530

    Added spies

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,7 +5,39 @@ import { prismaClient } from "../__mocks__/db";
 
 vi.mock("../db");
 
+
+//By using spies in addition to mocks, you can ensure that your tests not only verify the expected output 
+//but also validate that the functions or modules under test are called with the correct input values. 
+//This approach helps catch flaky tests and improves the reliability and accuracy of your test suite.
+ 
 describe("POST /sum", () => {
+  it("should return the sum of two numbers with input checking", async () => {
+    prismaClient.sum.create.mockResolvedValue({
+      id: 1,
+      a: 1,
+      b: 1,
+      result: 3
+    });
+    // Create a spy on the prismaClient.sum.create function.
+    vi.spyOn(prismaClient.sum, "create");
+
+    const res = await request(app).post("/sum").send({
+      a: 1,
+      b: 2
+    });
+    //Assert that the function was called with the correct input values
+    expect(prismaClient.sum.create).toHaveBeenCalledWith({
+      data: {
+        a: 1,
+        b: 2,
+        result: 3
+      }
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.answer).toBe(3);
+  });
+
   it("should return the sum of two numbers", async () => {
     prismaClient.sum.create.mockResolvedValue({
       id: 5,
